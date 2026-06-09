62c6f48dea8016a965a175bd8bb5193b53e2a077
Konsti7777 <Konstantin.Lauer@gmail.com>
2026-06-09 15:38:15 +0200

    chatti

--- Praktikum/drive.c
+++ Praktikum/drive.c
@@ -5,28 +5,60 @@
 
 static uint16_t initialHeading;
 
+static uint8_t avoiding = 0;
+static uint32_t avoidTimer = 0;
+static uint8_t avoidDirection = 0; // 0 = links, 1 = rechts
+
 void driveInit(void){
 	
 	initialHeading = CMPS14_GetHeading();
 	
 }
 
-void move(uint16_t speed){
-			
-	
-		
-			Motor_DriveForward(speed);
-			
-			if ( 300 > SonicGetDistanceMiddle()){
-				if(SonicGetDistanceLeft() < SonicGetDistanceRight()){
-					Motor_TurnCenteredClockwise(20);
-				}else{
-					Motor_TurnCenteredCounterClockwise(20);
-				}
-			}
-			
-			
-			
+void move(uint16_t speed)
+{
+    // Hindernis wird aktuell umfahren
+    if(avoiding)
+    {
+        avoidTimer++;
+
+        if(avoidDirection == 0)
+        {
+            Motor_TurnCenteredCounterClockwise(20);
+        }
+        else
+        {
+            Motor_TurnCenteredClockwise(20);
+        }
+
+        // Nach ca. 2 Sekunden wieder gerade fahren
+        if(avoidTimer > 200)
+        {
+            avoiding = 0;
+            avoidTimer = 0;
+
+            Motor_DriveForward(speed);
+        }
+
+        return;
+    }
+
+    // Normalfahrt
+    Motor_DriveForward(speed);
+
+    if(SonicGetDistanceMiddle() < 300)
+    {
+        avoiding = 1;
+
+        if(SonicGetDistanceLeft() < SonicGetDistanceRight())
+        {
+            avoidDirection = 1; // rechts drehen
+        }
+        else
+        {
+            avoidDirection = 0; // links drehen
+        }
+    }
 }
 	
 	
